881d587d2fe920b0527e1fc56f59f3e92fcbd2ce
benleung <cpleungaa@stu.ust.hk>
2014-04-15 19:27:36 +0800

    testing statistic

--- application/controllers/statistics.php
+++ application/controllers/statistics.php
@@ -64,7 +64,7 @@ class Statistics extends REST_Controller {
 		foreach($results as $result)
 			$this->core_controller->add_return_data($result["order_date"],$result["freq"]);*/	//hard-coded field name here
 			
-		$this->core_controller->add_return_data('a','b')->successfully_processed();
+		$this->core_controller->add_return_data('result',array('1'=>'haha','2'=>'hoho'))->successfully_processed();
 			
 		//$this->successfully_processed();
 	}
