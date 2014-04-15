116d04516de4f71108cf3e0fea253c04822fa2a6
Jason Ng <ywng@ust.hk>
2014-04-15 16:59:49 +0800

    fix bug

--- application/controllers/trip.php
+++ application/controllers/trip.php
@@ -262,7 +262,7 @@ class Trip extends REST_Controller {
 		$criteria = array(
                 $this->order_model->KEY_rating_session_key => md5($this->input->post('date_time')),
                 $this->order_model->KEY_oid => $oid,
-                $this->order_model->KEY_rating_score => "NULL",
+                $this->order_model->KEY_rating_score => "-1",
        	);
 
        	$update_data = array(
