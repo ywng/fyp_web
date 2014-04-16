2a5f3b76416a3ecacadb2c9806126f67ea1033b3
Jason Ng <ywng@ust.hk>
2014-04-16 21:20:43 +0800

    .

--- webpages/js/fetchStatData.js
+++ webpages/js/fetchStatData.js
@@ -18,68 +18,33 @@
  * The Hong Kong University of Science and Technology
  * Data Visualization, CSE, HKUST
  */
- 
-//The main data array of the graph
-//format:
-//data2 (List of fundObj)
-//   fundObj (Name, priceList)
-//      Name
-//      priceList (List of priceItem)
-//         priceItem (date, price)
-var data2;
 
-var parseDate = d3.time.format("%Y%m%d").parse;
-var fundName=new Array();
-var DateMapIndex=d3.map();
+var DayHourHeatmap=new Array();;
 
 function _init(){
 	// fetch data from database
 	//should have some sort of API for getting the data
 	$.ajax({
-		url: "http://mpf.vis.ywng.cloudbees.net/AllUnitPrice",
+		url: serverDomain+"index.php/statistics/getAllOrderHourWeek",
 		context: document.body,
 		dataType: "json", 
 		headers : {Accept : "application/json","Access-Control-Allow-Origin" : "*"},
 		type: 'GET', 
 		async: false,
 		success: function(data, textStatus, jqXHR){
-			data2=new Array();
-			for(var i=0; i < data.length; i++) {
-				var fund=new Object();
-				fund.vis="0";
-				fund.name=data[i][0].name;
-				fundName[i]=fund.name;
-				fund.priceList=new Array();
-				var fundData = new Array();
-				fundData=data[i][1].data;
-				for(var j=0;j<fundData.length;j++){
-					var dailyPrice=new Object();
-					dailyPrice.price=parseFloat(fundData[j].price);
-					var Str="";
-					Str+=fundData[j].year;
-					if(fundData[j].month<10){
-						Str+="0"+fundData[j].month;
-					}else{
-						Str+=fundData[j].month;
-					}
-					if(fundData[j].day<10){
-						Str+="0"+fundData[j].day;
-					}else{
-						Str+=fundData[j].day;
-					}
-					
-					//construct the map for mapping date to array index
-					//only do it once for first series is okay
-					if(i==0){
-						DateMapIndex.set(Str,j);
-					}
-					
-					dailyPrice.date= parseDate(Str);
-					fund.priceList[j]=dailyPrice;
-				}
-				data2[i]=fund;
+			var resultArray=data.result;
+			for(var i=0;i<resultArray.length;i++){
+				var dataPt=new Array();
+				dataPt[0]=resultArray[i].weekay;
+				dataPt[1]=resultArray[i].hour;
+				dataPt[2]=resultArray[i].freq;
+
+				DayHourHeatmap[i]=dataPt;
 			}
-			data2[0].vis="1";//when start, only visible the first one, other let user set it.	
+
+			for(var i=0;i<resultArray.length;i++){
+				console.log(resultArray[i][0]+ "  "+resultArray[i][1]+"  "+resultArray[i][2]);
+			}
 		},
 		error: function(jqHXR, textStatus, errorThrown) {
 			console.log('ajax error in get survey ID call:' +textStatus + ' ' + errorThrown);
@@ -89,32 +54,3 @@ function _init(){
 
 }
 
-function findMaxY(){
-	var max=-9999999;
-	for(var i=0; i < data2.length; i++) {
-		if(data2[i].vis=="1"){//only find within those selected fund sets
-			var fundData=data2[i].priceList;		
-			for(var j=0;j<fundData.length;j++){
-				if(fundData[j].price>max){
-					max=fundData[j].price;
-				}
-			}
-		}
-	}
-	return max;
-}
-
-function findMinY(){
-	var min=9999999;
-	for(var i=0; i < data2.length; i++) {
-		if(data2[i].vis=="1"){
-			var fundData=data2[i].priceList;		
-			for(var j=0;j<fundData.length;j++){
-				if(fundData[j].price<min){
-					min=fundData[j].price;
-				}
-			}
-		}
-	}
-	return min;
-}
