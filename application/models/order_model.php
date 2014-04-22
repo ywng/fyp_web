96a344dc5bac7d922b6d585a1755d25b9b08a3ca
ustbenchan <ustbenchan@gmail.com>
2014-04-22 21:36:38 +0800

    removed unwanted order

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -236,6 +236,7 @@ class Order_model extends CI_Model {
 						->join($this->Table_name_active, $this->Table_name_active.'.'.$this->KEY_oid
 							.'='.$this->Table_name_assigned.'.'.$this->KEY_oid)
 						->where($this->Table_name_assigned.'.'.$key, $value)
+						->where($this->Table_name_active.'.'.$this->KEY_status_id, $this->Status_KEY_pending)
 						->order_by($this->KEY_assigned_time, 'DESC')
 						->limit($limit, $offset)->get();
 
